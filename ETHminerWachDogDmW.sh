315804f7a934b119c52070f7db96dd679ab1d6b5
DeadManWalking <34924727+DeadManWalkingTO@users.noreply.github.com>
2018-02-17 15:36:33 +0200

    Simple works better

--- ETHminerWachDogDmW.sh
+++ ETHminerWachDogDmW.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
+
 #========== Start ==========
 
 clear
-V=1.5.2
+V=1.7.1
 echo '###############################################################################'
 echo '#                                                                             #'
 echo '#  ETHminerWachDogDmW Version '$V'                                           #'
@@ -11,15 +13,27 @@ echo '#                                                                         
 echo '###############################################################################'
 echo #
 
-#========== Initializing ==========
 
-# Initializing CUDA Devices Number @ Start
-nvidia-smi --format=csv,noheader,nounits --query-gpu=utilization.gpu > CUDA.log
-a=($(wc CUDA.log))
-CUDAnumber=${a[0]}
-#words=${a[1]}
-#chars=${a[2]}
-echo System CUDA GPUs are $CUDAnumber.
+#========== Run EthMiner Command ==========
+
+RunEthMinerCommand()
+{
+
+#==================== Your Code Start Here ====================
+
+#setx GPU_FORCE_64BIT_PTR 0
+#setx GPU_MAX_HEAP_SIZE 100
+#setx GPU_USE_SYNC_OBJECTS 1
+#setx GPU_MAX_ALLOC_PERCENT 100
+#setx GPU_SINGLE_ALLOC_PERCENT 100
+
+./ethminer -RH -U -S eu1.ethermine.org:4444 -O 0x7013275311fc37ccc1e40193D75086293eCb43A4.test
+
+#==================== Your Code End Here ====================
+
+}
+
+#========== Initializing ==========
 
 #========== Run Program ==========
 
@@ -30,18 +44,6 @@ while $ProgramError=0; do
 
 loopnum=$((loopnum+1))
 pdate=`date '+%Y-%m-%d %H:%M'`
-
-if [ $loopnum > 1 ]
-then
-nvidia-smi --format=csv,noheader,nounits --query-gpu=utilization.gpu > CUDA.log
-a=($(wc CUDA.log))
-CUDAnumberCheck=${a[0]}
-ProgramError=1
-fi
-
-# If NO Error Run Normal
-if [ ProgramError=0 ]
-then
 
 #========== Screen Output ==========
 
@@ -60,16 +62,15 @@ echo >> RunTimes.log
 
 #========== Execution Code ==========
 
-#setx GPU_FORCE_64BIT_PTR 0
-#setx GPU_MAX_HEAP_SIZE 100
-#setx GPU_USE_SYNC_OBJECTS 1
-#setx GPU_MAX_ALLOC_PERCENT 100
-#setx GPU_SINGLE_ALLOC_PERCENT 100
+RunEthMinerCommand
 
-./ethminer -RH -U -S eu1.ethermine.org:4444 -O 0x7013275311fc37ccc1e40193D75086293eCb43A4.test
-
+#Wait 5s
 sleep 5
 
+#Check 10 loops
+if [ $loopnum > 10 ]
+then
+ProgramError=1
 fi
 
 done
@@ -82,8 +83,11 @@ echo
 echo '###############################################################################'
 echo $pdate
 echo 'ETHminerWachDogDmW has run '$loopnum' times.'
-echo 'System CUDA GPU(s) '$CUDAnumber'. Working CUDA GPU(s) '$CUDAnumberCheck' .'
 echo 'System Restart Required.'
+echo
+echo
+echo
+echo 'Reboot Now ('$pdate')'
 echo '###############################################################################'
 echo
 
@@ -91,8 +95,11 @@ echo
 
 echo $pdate >> RunTimes.log
 echo 'ETHminerWachDogDmW has run '$loopnum' times.' >> RunTimes.log
-echo 'System CUDA GPU(s) '$CUDAnumber'. Working CUDA GPU(s) '$CUDAnumberCheck' .' >> RunTimes.log
 echo 'System Restart Required.' >> RunTimes.log
+echo >> RunTimes.log
+echo >> RunTimes.log
+echo >> RunTimes.log
+echo 'Reboot Now ('$pdate')' >> RunTimes.log
 echo >> RunTimes.log
 
 #========== System Reboot ==========
